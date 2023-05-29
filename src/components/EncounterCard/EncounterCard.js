fb84064b018acdfac55445cc6be80ff1bff60797
Pawel Ignaczak <ignak265@gmail.com>
2023-05-29 20:20:32 +0200

    Graphic lifting v1

--- src/components/EncounterCard/EncounterCard.js
+++ src/components/EncounterCard/EncounterCard.js
@@ -9,15 +9,24 @@ function EncounterCard({ setPage }) {
     return (
         <>
             <div className='encounter_card flex-row-center-center'>
+                <div className='top-of-pokedex flex-row-center-center'>
+                    <div className='blue-circle' />
+                    <div className='red-circle' />
+                    <div className='yellow-circle' />
+                    <div className='green-circle' />
+                </div>
                 <div className='pokemon_data flex-row-center-center'>
-                    <div className='stats'>
-                        <ul>
-                            <li>HP: 21</li>
-                            <li>Attack: 3</li>
-                            <li>Def: 7</li>
-                        </ul>
+                    <div className='pokemon-name'>Piłkaczu</div>
+                    <div className='details flex-row-center-center'>
+                        <div className='stats'>
+                            <ul>
+                                <li>HP: 21</li>
+                                <li>Attack: 3</li>
+                                <li>Def: 7</li>
+                            </ul>
+                        </div>
+                        <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' />
                     </div>
-                    <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'/>
                 </div>
                 <button className='battle_button'>Battle</button>
                 <button className='back_button' onClick={returnToMainPage}>Back</button>
